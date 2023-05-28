09ee16072893c6fd30891c571b818f9f1c510a14
marceloaguillar <marceloaguillarjr@gmail.com>
2023-05-27 21:30:30 -0300

    atualização fix: foi melhorado o modo de tratar os dados retirados do mercado livre, e foi incluido um filtro para os resultados da pesquisa, porém ele precisa ser melhorado

--- estruturacao.py
+++ estruturacao.py
@@ -1,60 +1,82 @@
+import requests
+from bs4 import BeautifulSoup
+
 #ACESSAR PLANILHA COM OS PRODUTOS
 import gspread
 import pandas as pd
 import numpy as np
 from oauth2client.service_account import ServiceAccountCredentials
 
-# Configuração das credenciais
+
+#CREDENCIAIS GOOGLE DRIVE
 scope = ['https://spreadsheets.google.com/feeds',
          'https://www.googleapis.com/auth/drive']
 credentials = ServiceAccountCredentials.from_json_keyfile_name('C:\\Users\\positivo\\Documents\\python\\projeto_mae\\projeto-mae-387416-f90f9773ee1f.json', scope)
-
-# Autenticação
 client = gspread.authorize(credentials)
 
-# Abertura da planilha
-planilha = client.open('pesquisa_fornecedores_teste').sheet1
 
-#Leitura de dados
-dados = planilha.get_all_values()[:17]
+#LEITURA DOS DADOS
+planilha = client.open('pesquisa_fornecedores_teste')
+pagina2 = planilha.get_worksheet(1)
 
+dados2 = pagina2.get_all_values()[:17]
+
+df2 = pd.DataFrame(dados2[1:], columns=dados2[0])
 pd.set_option('display.max_rows', None)
-df = pd.DataFrame(dados[1:], columns=dados[0])
-with pd.option_context('display.max_rows', None):
-    print(df.to_string(index=False))
-produto1 = df.loc[5, 'DESCRIÇÃO']
+print(df2)
+produto1 = df2.loc[7, 'DESCRIÇÃO']
+marca1 = df2.loc[7, 'MARCA']
+descricao = df2['DESCRIÇÃO'].tolist()
+produto_nome = f'{produto1} {marca1}'
 
-#MPORTAÇÃO DO SELENIUM PARA PESQUISA EM ALGUM SITE
-import time
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-driver = webdriver.Chrome()
+print('\n',produto_nome,'\n')
 
-#PESQUISAR OS PRODUTOS EM ALGUM LUGAR
-driver.get("https://www.mercadolivre.com.br/")
-input_element = driver.find_element(By.CSS_SELECTOR, 'input#cb1-edit')
-input_element.send_keys(f"{produto1}")
-input_element.send_keys(Keys.RETURN)
-nome = driver.find_element(By.XPATH, '//*[@id="root-app"]/div/div[2]/section/ol/li[1]/div/div/div[2]/div[1]/a/h2').text
-valor1 = driver.find_element(By.XPATH, '//*[@id="root-app"]/div/div[2]/section/ol/li[1]/div/div/div[2]/div[2]/div[1]/div/div/div/div/span[1]/span[2]/span[1]').text
-valor2 = driver.find_element(By.XPATH, '//*[@id="root-app"]/div/div[2]/section/ol/li[1]/div/div/div[2]/div[2]/div[1]/div/div/div/div/span[1]/span[2]/span[2]').text
-valor3 = driver.find_element(By.XPATH, '//*[@id="root-app"]/div/div[2]/section/ol/li[1]/div/div/div[2]/div[2]/div[1]/div/div/div/div/span[1]/span[2]/span[3]').text
-valor4 = driver.find_element(By.XPATH, '//*[@id="root-app"]/div/div[2]/section/ol/li[1]/div/div/div[2]/div[2]/div[1]/div/div/div/div/span[1]/span[2]/span[4]').text
-
-driver.quit()
-valor_final = f'{valor1} {valor2}{valor3}{valor4}'
-print(nome)
-print(valor_final)
-#MERCADO LIVRE
-#TENTAR MESCLAR SELENIUM COM PYAUTOGUI PARA FAZER A PESQUISA NO MERCADO LIVRE
+#PESQUISA NO MERCADO LIVRE
+url_padrao = 'https://lista.mercadolivre.com.br/'
+response = requests.get(url_padrao + produto_nome)
+site = BeautifulSoup(response.text, 'html.parser')
+produtos = site.findAll('div', attrs={'class': 'ui-search-result__wrapper'})
+palavras_evitadas = ['2 em 1', 'Kit', 'C/', '+',]
+carrinho = []
+#PESQUISA E COLETA DE DADOS DOS PRODUTOS
+contador = 0
+for produto in produtos:
+    if contador == 20:
+        break
+    titulo_principal = produto.find('a', attrs={'class': 'ui-search-item__group__element shops__items-group-details ui-search-link'})
+    titulo = titulo_principal.find('h2').text.strip()
+    link = produto.find('a', attrs={'class': 'ui-search-link'})
+    preco_principal = produto.find('div', attrs={'class': 'ui-search-price__second-line shops__price-second-line'})
+    valor = preco_principal.find('span', attrs={'class': 'price-tag-fraction'}).text
+    centavos = preco_principal.find('span', attrs={'class': 'price-tag-cents'})
 
 
-#RETIRAR O VALOR DOS PRODUTOS
-#JOGAR NA PLANILHA
-#ANALISAR DADOS COLETADOS
+    
+    palavras_pesquisa = produto_nome.split()
+    for item in palavras_pesquisa:
+        if item in titulo:
+            print(titulo)
+            if (centavos): #ISSO SIGNIFICA QUE SE O 'CENTAVOS' NÃO FOR NONE
+                print(f'R$ {valor},{centavos.text}')
+                valor_final = f'R$ {valor},{centavos.text}'
+                carrinho.append({titulo: valor_final})
+            else:
+                print(f'R$ {valor}')
+                carrinho.append({titulo: f'R$ {valor},00'})
+            
+            contador += 1
+
+        else:
+            continue
+            
 
 
-#https://lista.mercadolivre.com.br/l%C3%A1pis-cor-color-case-c%2F-12-cores-%C3%BE-estojo-%C3%BE-apontador-c%2F-dep%C3%B3sito-cis#D[A:L%C3%A1pis%20Cor%20Color%20Case%20c/%2012%20Cores%20+%20Estojo%20+%20Apontador%20c/%20Dep%C3%B3sito%20-%20CIS]
+        print('\n')
+
+    #MELHORAR O MODO DE PESQUISA, TALVE CRIAR UM NOVO FILTRO PARA O DICIONARIO CARRINHO, EXCLUINDO OS ITENS COM AS PALAVRAS EVITADAS
+    #MANTENDO UMA EXTRAÇÃO DE 20 ITENS, PODE SER UMA MARGEM BOA PARA APÓS O FILTRO MANTER OS 10 ITENS NECESSÁRIOS
+
+
+print(carrinho)
 
 
